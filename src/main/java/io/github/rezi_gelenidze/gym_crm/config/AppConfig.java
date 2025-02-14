4f1738b29a223345424d829f75700fcb61a8594c
rezi-gelenidze <rezi.gelenidze7@gmail.com>
2025-02-14 20:13:35 +0400

    define appconfig for exploring components

---
+++ src/main/java/io/github/rezi_gelenidze/gym_crm/config/AppConfig.java
@@ -0,0 +1,9 @@
+package io.github.rezi_gelenidze.gym_crm.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "io.github.rezi_gelenidze.gym_crm")
+public class AppConfig {
+}
