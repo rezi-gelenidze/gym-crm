890d6d2870e2711a0668bc5b12f7b2a9a6b0d0cd
rezi-gelenidze <rezi.gelenidze7@gmail.com>
2025-02-18 21:21:22 +0400

    edit annotation as service

--- src/main/java/io/github/rezi_gelenidze/gym_crm/service/UserService.java
+++ src/main/java/io/github/rezi_gelenidze/gym_crm/service/UserService.java
@@ -9,9 +9,9 @@ import io.github.rezi_gelenidze.gym_crm.entity.Trainer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
-@Repository
+@Service
 public class UserService {
     PasswordEncoder passwordEncoder;
 
