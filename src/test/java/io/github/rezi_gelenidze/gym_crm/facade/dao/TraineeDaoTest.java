432629568c80f686d1e7d96040bb77df66c006db
rezi-gelenidze <rezi.gelenidze7@gmail.com>
2025-02-19 12:11:32 +0400

    implement unit tests for Daos

---
+++ src/test/java/io/github/rezi_gelenidze/gym_crm/facade/dao/TraineeDaoTest.java
@@ -0,0 +1,100 @@
+package io.github.rezi_gelenidze.gym_crm.facade.dao;
+
+
+import io.github.rezi_gelenidze.gym_crm.dao.TraineeDao;
+import io.github.rezi_gelenidze.gym_crm.entity.Trainee;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TraineeDaoTest {
+    private TraineeDao traineeDao;
+
+    @BeforeEach
+    public void setUp() {
+        // Manually injecting a ConcurrentHashMap to the TrainerDao
+        this.traineeDao = new TraineeDao(new ConcurrentHashMap<>());
+    }
+
+    // Helper method to create a Trainee object
+    private Trainee createDummyTrainee() {
+        return new Trainee(
+                1L,
+                "Alice",
+                "Johnson",
+                "Alice.Johnson",
+                "password",
+                "1995-04-20",
+                "123 Main St"
+        );
+    }
+
+    @Test
+    public void testSaveTrainee() {
+        Trainee trainee = this.createDummyTrainee();
+
+        traineeDao.saveTrainee(trainee);
+        Optional<Trainee> retrievedTrainee = traineeDao.getTrainee(1L);
+
+        assertTrue(retrievedTrainee.isPresent());
+        assertEquals(trainee, retrievedTrainee.get());
+    }
+
+    @Test
+    public void getTrainee_WhenTraineeDoesNotExist_ReturnsEmptyOptional() {
+        Optional<Trainee> retrievedTrainee = traineeDao.getTrainee(1L);
+
+        assertTrue(retrievedTrainee.isEmpty());
+    }
+
+    @Test
+    public void getTrainee_WhenTraineeExists_ReturnsTrainee() {
+        Trainee trainee = this.createDummyTrainee();
+        traineeDao.saveTrainee(trainee);
+
+        Optional<Trainee> retrievedTrainee = traineeDao.getTrainee(1L);
+
+        assertTrue(retrievedTrainee.isPresent());
+        assertEquals(trainee, retrievedTrainee.get());
+
+    }
+
+    @Test
+    public void testUpdateTrainee_WhenTraineeExists_DoNotThrowException() {
+        Trainee trainee = this.createDummyTrainee();
+        traineeDao.saveTrainee(trainee);
+
+        trainee.setAddress("Wall St");
+
+        assertDoesNotThrow(() -> traineeDao.updateTrainee(trainee));
+
+        Optional<Trainee> retrievedTrainee = traineeDao.getTrainee(1L);
+        assertTrue(retrievedTrainee.isPresent());
+
+        assertEquals("Wall St", retrievedTrainee.get().getAddress());
+    }
+
+    @Test
+    public void testUpdateTrainee_WhenTraineeDoesNotExist_ThrowsIllegalArgumentException() {
+        Trainee trainee = this.createDummyTrainee();
+
+        assertThrows(IllegalArgumentException.class, () -> traineeDao.updateTrainee(trainee));
+    }
+
+    @Test
+    public void testDeleteTrainee_WheTraineeExists_ReturnsTrue() {
+        Trainee trainee = this.createDummyTrainee();
+        traineeDao.saveTrainee(trainee);
+
+        assertTrue(traineeDao.deleteTrainee(1L));
+    }
+
+    @Test
+    public void testDeleteTrainee_WhenTraineeDoesNotExist_ReturnsFalse() {
+        assertFalse(traineeDao.deleteTrainee(1L));
+    }
+}
---
+++ src/test/java/io/github/rezi_gelenidze/gym_crm/facade/dao/TrainerDaoTest.java
@@ -0,0 +1,84 @@
+package io.github.rezi_gelenidze.gym_crm.facade.dao;
+
+import io.github.rezi_gelenidze.gym_crm.dao.TrainerDao;
+import io.github.rezi_gelenidze.gym_crm.entity.Trainer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TrainerDaoTest {
+    private TrainerDao trainerDao;
+
+    @BeforeEach
+    public void setUp() {
+        // Manually injecting a ConcurrentHashMap to the TrainerDao
+        this.trainerDao = new TrainerDao(new ConcurrentHashMap<>());
+    }
+
+    // Helper method to create a Trainer object
+    private Trainer createDummyTrainer() {
+        return new Trainer(
+                1L,
+                "John",
+                "Doe",
+                "John.Doe",
+                "password",
+                "Cardio"
+        );
+    }
+
+    @Test
+    public void testSaveTrainer() {
+        Trainer trainer = this.createDummyTrainer();
+
+        trainerDao.saveTrainer(trainer);
+        Optional<Trainer> retrievedTrainer = trainerDao.getTrainer(1L);
+
+        assertTrue(retrievedTrainer.isPresent());
+        assertEquals(trainer, retrievedTrainer.get());
+    }
+
+    @Test
+    public void getTrainer_WhenTrainerDoesNotExist_ReturnsEmptyOptional() {
+        Optional<Trainer> retrievedTrainer = trainerDao.getTrainer(1L);
+
+        assertTrue(retrievedTrainer.isEmpty());
+    }
+
+    @Test
+    public void getTrainer_WhenTrainerExists_ReturnsTrainer() {
+        Trainer trainer = this.createDummyTrainer();
+        trainerDao.saveTrainer(trainer);
+
+        Optional<Trainer> retrievedTrainer = trainerDao.getTrainer(1L);
+
+        assertTrue(retrievedTrainer.isPresent());
+        assertEquals(trainer, retrievedTrainer.get());
+    }
+
+    @Test
+    public void testUpdateTrainer_WhenTrainerExists_DoNotThrowException() {
+        Trainer trainer = this.createDummyTrainer();
+        trainerDao.saveTrainer(trainer);
+
+        trainer.setSpecialization("Cardio");
+
+        assertDoesNotThrow(() -> trainerDao.updateTrainer(trainer));
+
+        Optional<Trainer> retrievedTrainer = trainerDao.getTrainer(1L);
+        assertTrue(retrievedTrainer.isPresent());
+
+        assertEquals("Cardio", retrievedTrainer.get().getSpecialization());
+    }
+
+    @Test
+    public void testUpdateTrainer_WhenTrainerDoesNotExist_ThrowIllegalArgumentException() {
+        Trainer trainer = this.createDummyTrainer();
+
+        assertThrows(IllegalArgumentException.class, () -> trainerDao.updateTrainer(trainer));
+    }
+}
