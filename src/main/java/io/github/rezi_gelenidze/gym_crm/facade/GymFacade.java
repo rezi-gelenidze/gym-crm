98f4917f037e8a412cf28ca46b798123f3351209
rezi-gelenidze <rezi.gelenidze7@gmail.com>
2025-03-06 20:22:32 +0400

    enforde validation with @Valid in facade layer

--- src/main/java/io/github/rezi_gelenidze/gym_crm/facade/GymFacade.java
+++ src/main/java/io/github/rezi_gelenidze/gym_crm/facade/GymFacade.java
@@ -8,6 +8,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -19,75 +20,75 @@ public class GymFacade {
     private final UserService userService;
     private final TrainingService trainingService;
 
-    public Trainer createTrainer(TrainerDto trainerDto) {
+    public Trainer createTrainer(@Valid TrainerDto trainerDto) {
         return trainerService.createTrainer(trainerDto);
     }
 
-    public Trainee createTrainee(TraineeDto traineeDto) {
+    public Trainee createTrainee(@Valid TraineeDto traineeDto) {
         return traineeService.createTrainee(traineeDto);
     }
 
-    public Optional<Trainer> getTrainerByUsername(CredentialsDto credentials, String queryUsername) {
+    public Optional<Trainer> getTrainerByUsername(@Valid CredentialsDto credentials, String queryUsername) {
         userService.authenticate(credentials);
 
         return trainerService.getTrainerByUsername(queryUsername);
     }
 
-    public Optional<Trainee> getTraineeByUsername(CredentialsDto credentials, String queryUsername) {
+    public Optional<Trainee> getTraineeByUsername(@Valid CredentialsDto credentials, String queryUsername) {
         userService.authenticate(credentials);
 
         return traineeService.getTraineeByUsername(queryUsername);
     }
 
-    public void updatePassword(CredentialsDto credentials, String newPassword) {
+    public void updatePassword(@Valid CredentialsDto credentials, @Valid String newPassword) {
         userService.authenticate(credentials);
 
         userService.updatePassword(credentials.getUsername(), newPassword);
     }
 
-    public Trainer updateTrainerProfile(CredentialsDto credentials, TrainerUpdateDto trainerUpdateDto) {
+    public Trainer updateTrainerProfile(@Valid CredentialsDto credentials, @Valid TrainerUpdateDto trainerUpdateDto) {
         userService.authenticate(credentials);
 
         return trainerService.updateTrainerProfile(trainerUpdateDto, credentials.getUsername());
     }
 
-    public Trainee updateTraineeProfile(CredentialsDto credentials, TraineeUpdateDto traineeUpdateDto) {
+    public Trainee updateTraineeProfile(@Valid CredentialsDto credentials, @Valid TraineeUpdateDto traineeUpdateDto) {
         userService.authenticate(credentials);
 
         return traineeService.updateTraineeProfile(traineeUpdateDto, credentials.getUsername());
     }
 
-    public void updateUserActiveStatus(CredentialsDto credentials, boolean active) {
+    public void updateUserActiveStatus(@Valid CredentialsDto credentials, boolean active) {
         userService.authenticate(credentials);
 
         userService.updateActiveStatus(credentials.getUsername(), active);
     }
 
-    public void deleteTrainee(CredentialsDto credentials) {
+    public void deleteTrainee(@Valid CredentialsDto credentials) {
         userService.authenticate(credentials);
 
         traineeService.deleteTrainee(credentials.getUsername());
     }
 
-    public List<Training> getTraineeTrainings(CredentialsDto credentials, String traineeUsername, LocalDate fromDate, LocalDate toDate, String trainerName, String trainingType) {
+    public List<Training> getTraineeTrainings(@Valid CredentialsDto credentials, String traineeUsername, LocalDate fromDate, LocalDate toDate, String trainerName, String trainingType) {
         userService.authenticate(credentials);
 
         return trainingService.getTraineeTrainings(traineeUsername, fromDate, toDate, trainerName, trainingType);
     }
 
-    public List<Training> getTrainerTrainings(CredentialsDto credentials, String trainerUsername, LocalDate fromDate, LocalDate toDate, String traineeName) {
+    public List<Training> getTrainerTrainings(@Valid CredentialsDto credentials, String trainerUsername, LocalDate fromDate, LocalDate toDate, String traineeName) {
         userService.authenticate(credentials);
 
         return trainingService.getTrainerTrainings(trainerUsername, fromDate, toDate, traineeName);
     }
 
-    public Training addTraining(CredentialsDto credentials, TrainingDto trainingDto) {
+    public Training addTraining(@Valid CredentialsDto credentials, @Valid TrainingDto trainingDto) {
         userService.authenticate(credentials);
 
         return trainingService.createTraining(trainingDto);
     }
 
-    public List<Trainer> getUnassignedTrainers(CredentialsDto credentials, String traineeUsername) {
+    public List<Trainer> getUnassignedTrainers(@Valid CredentialsDto credentials, String traineeUsername) {
         userService.authenticate(credentials);
 
         return trainerService.getUnassignedTrainers(traineeUsername);
