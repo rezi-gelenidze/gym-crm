f4a91c9b198fce057a9b6b7dfe1ad83d2bddab19
rezi-gelenidze <rezi.gelenidze7@gmail.com>
2025-03-25 21:56:11 +0400

    configure spring boot for running web server

---
+++ src/main/java/io/github/rezi_gelenidze/gym_crm/GymCrmApplication.java
@@ -0,0 +1,11 @@
+package io.github.rezi_gelenidze.gym_crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymCrmApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GymCrmApplication.class, args);
+    }
+}
