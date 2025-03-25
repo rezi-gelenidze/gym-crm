90a4e2feef7b2aa8f0d6fa2e95132725922af4fd
rezi-gelenidze <rezi.gelenidze7@gmail.com>
2025-03-25 21:55:53 +0400

    factor out variables in properties

--- src/main/java/io/github/rezi_gelenidze/gym_crm/config/JpaConfig.java
+++ src/main/java/io/github/rezi_gelenidze/gym_crm/config/JpaConfig.java
@@ -32,9 +32,9 @@ public class JpaConfig {
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/gym");
-        dataSource.setUsername("admin");
-        dataSource.setPassword("adminpassword");
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
         return dataSource;
     }
 
