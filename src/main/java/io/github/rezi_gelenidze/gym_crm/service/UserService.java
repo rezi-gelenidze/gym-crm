7eb6dd9effae7cfa063c28187aa6d20882b7fb8a
rezi-gelenidze <rezi.gelenidze7@gmail.com>
2025-04-12 03:24:53 +0400

    implement services

--- src/main/java/io/github/rezi_gelenidze/gym_crm/service/UserService.java
+++ src/main/java/io/github/rezi_gelenidze/gym_crm/service/UserService.java
@@ -2,10 +2,6 @@ package io.github.rezi_gelenidze.gym_crm.service;
 
 import java.security.SecureRandom;
 
-import io.github.rezi_gelenidze.gym_crm.dto.auth.CredentialsDto;
-import io.github.rezi_gelenidze.gym_crm.entity.User;
-import io.github.rezi_gelenidze.gym_crm.exception.InvalidCredentialsException;
-import io.github.rezi_gelenidze.gym_crm.exception.UserNotFoundException;
 import io.github.rezi_gelenidze.gym_crm.repository.UserRepository;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,25 +19,6 @@ public class UserService {
     private final UserRepository userRepository;
 
     private final SecureRandom random = new SecureRandom();
-
-    // Authentication assertion, raises exception if authentication fails
-    public void authenticate(String username, String password) {
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(UserNotFoundException::new);
-        if (!passwordEncoder.matches(password, user.getPassword()))
-            throw new InvalidCredentialsException();
-    }
-
-    // DTO overloaded, for convenience
-    public void authenticate(CredentialsDto credentials) {
-        this.authenticate(credentials.getUsername(), credentials.getPassword());
-    }
-
-    public void assertIdentity(String clientUsername, String targetUsername) {
-        if (!clientUsername.equals(targetUsername)) {
-            throw new InvalidCredentialsException();
-        }
-    }
 
     public void updatePassword(String username, String newPassword) {
         userRepository.findByUsername(username).ifPresent(user -> {
