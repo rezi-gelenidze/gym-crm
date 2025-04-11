da9b89028291ec2b09dfca0c349035bb49116b25
rezi-gelenidze <rezi.gelenidze7@gmail.com>
2025-04-12 03:23:57 +0400

    update entity as UserDetails supported one

--- src/main/java/io/github/rezi_gelenidze/gym_crm/entity/User.java
+++ src/main/java/io/github/rezi_gelenidze/gym_crm/entity/User.java
@@ -2,13 +2,21 @@ package io.github.rezi_gelenidze.gym_crm.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED) // Ensures separate tables
+@Inheritance(strategy = InheritanceType.JOINED)
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
+    private enum Role { USER, ADMIN }
+
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long userId;
@@ -28,6 +36,10 @@ public class User {
     @Column(nullable = false)
     private boolean active;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role = Role.USER; // Default role is USER
+
     public User(String firstName, String lastName, String username, String password) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -35,4 +47,22 @@ public class User {
         this.password = password;
         this.active = true;
     }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    // For other UserDetails flags return active flag as indicator
+    @Override
+    public boolean isAccountNonExpired() { return this.active; }
+
+    @Override
+    public boolean isAccountNonLocked() { return this.active; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return this.active; }
+
+    @Override
+    public boolean isEnabled() { return this.active; }
 }
