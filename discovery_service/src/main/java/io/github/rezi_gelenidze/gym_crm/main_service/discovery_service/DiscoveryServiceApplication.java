352aebef31d1964380b813a9fdfea385d426f0c7
rezi-gelenidze <rezi.gelenidze7@gmail.com>
2025-05-28 15:06:54 +0400

    create a discovery eureka service

---
+++ discovery_service/src/main/java/io/github/rezi_gelenidze/gym_crm/main_service/discovery_service/DiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.rezi_gelenidze.gym_crm.main_service.discovery_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+}
